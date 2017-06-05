1d1ffb740eabf75ef47e24d1e87bb39406c99152
Wandeson Ricardo <wandeson.math@gmail.com>
2017-06-05 13:11:44 -0300

    Update src/ppm.py

--- src/ppm.py
+++ src/ppm.py
@@ -25,8 +25,8 @@ class PPM:
         self.mode = mode
         self.Img_file = None
         self.Img = None # self.Img = MatrixImage(self.dim,self.mode)
-        if file_name != None:
-            self.Img_file = open(name_file,'wr')
+        if self.name_file != None:
+            self.Img_file = open(self.name_file,'wr')
             #self.Img_file.write('P3\n'+' '+str(self.columm)+' '+str(self.row)+' 255\n')
             #self.putPixel = lambda Pixel: self.Img_file.write( (Pixel[i][j].r+' ' +
             #                                                   Pixel[i][j].g+' '+
@@ -37,7 +37,7 @@ class PPM:
         """
 		Open read file PPM and return image (matrixImage).
         """
-        i,j,k,c = 0,0,0,None
+        i, j, k, c = 0, 0, 0, None
         if imname != None:
             self.name_file= imname
             self.Img_file = open(self.name_file, 'r')
@@ -47,42 +47,76 @@ class PPM:
         im_temp = self.Img_file.read()
         img = im_temp.split('\n')
         self.mode = img[0]
+        img.pop(-1) # Remove element in index -1. (end list).
         temp = img[2].split() # dimension of image. It is in type 'string'. 'm n' in the line of file.
         self.dim = ( int( temp[0] ), int( temp[1] ) ) # Tuple with dimension of image (m,n); m rows and n collums.
         self.Img = MatrixImage( self.dim, self.mode )
+        print(type(img),img)
         img = map( lambda i: int(i), img[3:] ) # Convert of string to integer color pixel values from file.
         # Image mode color
         if self.mode == 'P3':
+            """
+            # First implementation to map pixels in file and set Matrix of image.
             # Reading content in file image PPM in list
             for c in img[3:]:
-                    # Pixels i,j
+                print(i,j)
+                # Pixels i,j
                 if k == 0:
-                    self.Img[i][j].r = c
-                    k += 1
+                    self.Img.Matrix[i][j].r = c
                     j += 1 # next element colum from line i
                 elif k == 1:
-                    self.Img[i][j] = c
-                    k += 1
+                    self.Img.Matrix[i][j].g = c                    
                     j += 1 
-                else:
+                elif k == 2:
+                    self.Img.Matrix[i][j].b = c
                     k = 0
-                    i += 1 # Next line i. After 3 channels R,G,B color if mode == 'color' (P3 header file PPM)
-
-                # Black and White color
-        else:
+                    j = 0
+                    i += 1
+                # Next channel rgb element value.
+                k += 1"""
+            #
+            # Reading content file in PPM and set Matrix image. Second Implementation.
+            # Start c in element 3 in the list img (list with rgb values of file PPM)
+            #
+            c = 3
+            # Map pixels from file with Img.
+            while i < self.dim[0]:
+                # Next channel rgb element value.
+                # k += 1 # if k start in -1
+                while j < self.dim[1]:
+                    # Pixels i,j
+                    
+                    if k == 0:
+                        self.Img.Matrix[i][j].r = img[c]
+                        j += 1
+                    elif k == 1:
+                        self.Img.Matrix[i][j].g = img[c]              
+                        j += 1
+                    elif k == 2:
+                        self.Img.Matrix[i][j].b = img[c]
+                        k = 0
+                # Next line i, set columm j = 0
+                i += 1
+                j = 0
+                # img
+                c += 1
+                # Next channel rgb element value
+                k += 1
+        # Black and White color
+        elif self.mode == 'P2':
 		
             # If image mode is black/white
             # c = 0
             for c in img[3:]:
-                if i > self.dim[0]:
+                if i >= self.dim[0]:
                     i += 1
                 else:
-                    self.Img[i][j] = c
+                    self.Img.Matrix[i][j] = c
                     j += 1
-				
-                    #c += 1	
-			
-        
+        else:
+            return 0
+        return 1
+
 
     def writePixel(self,Pixel,i,j):
         self.Pixel = Pixel
