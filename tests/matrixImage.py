aa19a15bdd13e89afa6aa1f1f11dc5e0ed7115b7
WSRicardo <wandeson.math@gmail.com>
2023-06-18 20:30:00 -0300

    Update. Organize project and clean folders.

--- tests/matrixImage.py
+++ tests/matrixImage.py
@@ -1,58 +1,73 @@
-#-------------------------------------------------------------------------------
+#!/usr/bin/env python3
+
+
 # Name:        matrixImage.py
 # Purpose:     Create matrix for image
 #
 # Author:      Wandeson Ricardo
-#
 # Created:     2013
-# Copyright:   (c) 2012
+# Copyright:   (c) 2013
 # Licence:     GPL
-#-------------------------------------------------------------------------------
-#!/usr/bin/env python
+# Blog:		www.wanartsci.blogspot.com
+#		www.gaenos.blogspot.com
+
+# Module to create and manipulate Image.
+# Function lambda to create matrix with 0's.
+# Matrix = lambda m, n: map(lambda k: Vector(n), range(0, m))
+# Function to create matrix for image with chanel RGB.
+MatrixImRGB = lambda m, n: map(lambda k: map(lambda k: RGB(), range(0, n)), range(0, m))
 
 
-#Module to create and manipulate Image.
-
-#Function lambda to create matrix with 0's.
-Matrix = lambda m,n: map(lambda k:  Vector(n), range(0,m))
-#Function to create matrix for image with chanel RGB.
-MatrixImRGB = lambda m,n: map( lambda k: map(lambda k: RGB(), range(0,n) ), range(0,m) )
-
-
-class matrixImage:
+class MatrixImage:
     """
         Class for build matrix structure for image.
-        Format supported to color in this momment RGB.
+        Format supported to color in this moment RGB.
     """
-    def __init__(self, dim, mode="rgb", fileFormat=None):
+
+    def __init__(self, dim, mode="rgb", fileformat=None):
+        # type: (object, object, object) -> object
+        """
+        Create Image object to manipulation images 
+        file ppm, color mode rgb.
+        
+        :rtype: object
+        :type dim: list, of length 2.
+        :type mode: object string.
+        :type fileformat string.
+
+        """
+
         self.dim = dim
         self.mode = mode
-        self.fileFormat = fileFormat
-        self.Matrix = MatrixImRGB(dim[0],dim[1])
-        if mode=="rgb":
-            print "RGB mode"
-
+        self.fileformat = fileformat
+        #self.Matrix = MatrixImRGB(dim[0], dim[1])
+        m, n = self.dim[0], self.dim[1]
+        self.matrix = [ [ RGB() for j in range(n) ] for i in range(m) ]
+        if mode == "rgb":
+            print("RGB mode")
+        elif not mode.lower() in ('rgb','gray'):
+            print("Modes: rgb color or gray scale")
 
 
 class RGB:
-    def __init__(self, r=0,g=0,b=0):
+    def __init__(self, r=0, g=0, b=0):
         self.r = r
         self.b = b
         self.g = g
 
-    def centralizeRGB(self):
+    def centralizergb(self):
         if self.r < 0:
-            self.r=0
+            self.r = 0
         elif self.r > 255:
-            self.r=255
+            self.r = 255
 
         if self.g < 0:
             self.g = 0
         elif self.g > 255:
-            self.g=255
+            self.g = 255
 
         if self.b < 0:
-            self.b= 0
+            self.b = 0
         elif self.b > 255:
             self.b = 255
 
--- tests/test.py
+++ tests/test.py
@@ -1,29 +1,13 @@
-# -- coding: latin-1 --
-from PPM import*
-from matrixImage import*
-from math import*
+import ppm
+import matrixImage
 
-dim = (600, 800)
-I = matrixImage(dim)
-#mapear i gerando (i=0,1,2,...dim[0]) e para cada i calcular f(i)=y,onde x=j e y = i. 
-#antes deslocar todos os x,y em k_x e k_y unidades afim de centralizar o gráfico.
-k_j, k_i  = 300, 400
-f = lambda x: sin(x)
-def drawGraph(f,intervalo_x):
-    """
-    f is function
-    intervalo_x lista (k1,k2) intervalos das coordenadas x.
-    """
+#im = matrixImage.MatrixImage((2,2)).matrix
+#print(im)
+#img_filename = "test.ppm"
+#img_file = ppm.PPM(img_filename,2,2)
+#img_file.write(im)
 
-file_name="test.ppm"
-I2PPM = PPM(file_name,dim[0],dim[1])
-for i in range(0,600):
-    I.Matrix[i][800/2].g = 255
-for j in range(0,800):
-    I.Matrix[600/2][j].r = 255
+# Test read image PPM
+im = ppm.PPM('test.ppm').iread()
 
-#teste requer correçoes
-for j in range(0,800):
-    I.Matrix[int(-j/2)][j].b = 255 
-I.Matrix[1][2].b=23
-I2PPM.write(I.Matrix)
+
